d392429d0547d71c39772c6879539f61bb882df1
johnedisc <christopher.johnedis@gmail.com>
2023-01-13 14:31:00 -0800

    read in user variable, create content to display to user after logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,34 @@
+function hide() {
+	
+}
 
 function formSubmit(e) {
 	e.preventDefault();
 	
-	//print the results to the DOM
-	let printEl = document.querySelector("#printEl h3");
+// read in user data
+	const name = document.querySelector("input#name").value
+	const time = document.querySelector("input#time").value
+	const menu = document.querySelector("select#menu").value
+	const speed = document.querySelector("input[name='speed']:checked").value
+	const workType = document.querySelector("input[name='workType']:checked").value
+	const pain = document.querySelector("input[name='pain']:checked").value
+	console.log(menu);
+	console.log(speed);
+	console.log(workType);
+	console.log(pain);
+
+// select hidden lang <divs>
+	const go = document.querySelector("div#go");
+	const rust = document.querySelector("div#rust");
+	const ts = document.querySelector("div#ts");
+	
+//	print the results to the DOM
+	const printEl = document.querySelector("#printEl h2");
 	printEl.innerText = "hello";
+	go.removeAttribute("class");
+	ts.removeAttribute("class");
+	rust.removeAttribute("class");
+	
 }
 
 window.addEventListener("load", function(e) {
